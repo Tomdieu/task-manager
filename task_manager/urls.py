c4b3df2d1bec8a3c4edd10eff0a2ad37a29f536e
tomdieu <ivantomdio@gmail.com>
2024-08-23 23:09:46 +0100

    Refactor docker-compose.yml and remove Elasticsearch configuration

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -17,6 +17,41 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Manager API",
+        default_version="v1",
+        description="Task manager api",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ivan.tomdieu@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
+    path("api/",include('task.urls'))
 ]
